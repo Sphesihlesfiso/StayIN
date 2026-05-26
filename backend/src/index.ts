94f4c6b77ccaf50ca4798a61eafbe64d998be3e1
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2026-05-26 11:36:45 +0200

    fix: remove frontend submodule

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import userRoutes from "./routes/user.routes"
 import { errorHandler } from "./middleware/errorHandler.error";
 
 configDotenv();
-const Port = process.env.PORT || 3000;
+const Port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
